867e8c4e178e00ea567ba2a258b5645ae2963bed
rafaelnet29 <rafaelsouzanet29@outlook.com>
2024-06-17 16:15:48 -0300

    - Foi trocada a tela de menu por uma tela gráfica

--- src/exemplo/funcoes/Main.java
+++ src/exemplo/funcoes/Main.java
@@ -1,46 +1,8 @@
 package exemplo.funcoes;
 
-import java.text.MessageFormat;
-import javax.swing.JOptionPane;
-
 public class Main {
 
     public static void main(String[] args) {
-        int opcao = 0;
-        int numero = 0;
-        double price, percent;
-        String palavra = "";
-        do {
-            // Ínicio do Menu
-            opcao = Integer.parseInt(JOptionPane.showInputDialog(null, """
-                                                                  -->> Informe sua Opção:
-                                                                 1 - FATORIAL
-                                                                 2 - PAR ou IMPAR
-                                                                 3 - DESCONTO
-                                                                 4 - JUROS
-                                                                 5 - PALÍNDROMO
-                                                                 0 - SAIR""", "Menu", 1));
-            if (opcao == 1) {
-                numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe um Número:", "FATORIAL", 1));
-                JOptionPane.showMessageDialog(null, MessageFormat.format("O Fatorial de {0} é: {1}", numero, new Funcoes(numero).RecursivaFatorial()), "FATORIAL", 1);
-            } else if (opcao == 2) {
-                numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe um Número:", "PAR ou IMPAR", 1));
-                new Funcoes(numero).FuncaoParOuImpar();
-            } else if (opcao == 3) {
-                price = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o preço do produto:", "DESCONTO", 1));
-                percent = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe a porcentagen de desconto:", "DESCONTO", 1));
-                new Funcoes(price, percent).aplicarDesconto();
-            } else if (opcao == 4) {
-                new Funcoes(Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o capital aplicado:", "JUROS SIMPLES", 1)),
-                        Double.parseDouble(JOptionPane.showInputDialog(null, "Informe a taxa de juros:", "JUROS SIMPLES", 1)),
-                        Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o tempo:", "JUROS SIMPLES", 1))).juroSimples();
-            } else if (opcao == 5) {
-                palavra = JOptionPane.showInputDialog(null, "Informe uma palavra para verificar se é ou não Palimdromo(a):", "Palíndroma", 1);
-                new Funcoes(palavra).palindroma();
-            } else {
-                JOptionPane.showMessageDialog(null, "Até a próxima!", "Menu", 1);
-            }
-
-        } while (opcao != 0);
+       new TelaMenu().setVisible(true);
     }
 }
